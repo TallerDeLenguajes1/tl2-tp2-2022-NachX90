c0f02245b7354ffd792de25d05012d628bf25c41
NachX90 <nachx90@protonmail.com>
2022-09-25 17:00:31 -0300

    LogTest

---
+++ LogTest/Program.cs
@@ -0,0 +1,17 @@
+﻿public static class Program
+{
+    private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+    public static void Main()
+    {
+        try
+        {
+           Logger.Info("Hello world");
+           System.Console.ReadKey();
+        }
+        catch (Exception ex)
+        {
+           Logger.Error(ex, "Goodbye cruel world");
+        }
+    }
+}  
