033dbc49bbfa344bb464d3f402584cbaea537940
NachX90 <nachx90@protonmail.com>
2022-10-02 01:33:55 -0300

    corrección namespace :s

--- Empleados/Program.cs
+++ Empleados/Program.cs
@@ -1,70 +1,68 @@
 ﻿using System;
 
-namespace Empleados
+namespace Empleados;
+static class Program
 {
-    static class Program
+    private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+    
+    public static int Main(string[] args)
     {
-        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
-        
-        public static int Main(string[] args)
+        List<Empleado> listaEmpleados = new List<Empleado>();
+        bool exito;
+        Mensajes.Titulo("BIENVENIDO");
+        do
         {
-            List<Empleado> listaEmpleados = new List<Empleado>();
-            bool exito;
-            Mensajes.Titulo("BIENVENIDO");
-            do
-            {
-                exito = CargarEmpleados(listaEmpleados);
-            } while (!exito);
-            MostrarEmpleados(listaEmpleados);
-            Console.WriteLine("\nGracias por utilizar el programa.");
-            Logger.Info("Programa finalizado correctamente :)");
-            return 0;
-        }
+            exito = CargarEmpleados(listaEmpleados);
+        } while (!exito);
+        MostrarEmpleados(listaEmpleados);
+        Console.WriteLine("\nGracias por utilizar el programa.");
+        Logger.Info("Programa finalizado correctamente :)");
+        return 0;
+    }
 
-        static bool CargarEmpleados(List<Empleado> listaEmpleados)
+    static bool CargarEmpleados(List<Empleado> listaEmpleados)
+    {
+        Mensajes.TerminarLinea("Carga de empleados");
+        try
         {
-            Mensajes.TerminarLinea("Carga de empleados");
-            try
+            Console.Write("Ingrese la cantidad de empleados a cargar: ");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= cantidad; i++)
             {
-                Console.Write("Ingrese la cantidad de empleados a cargar: ");
-                int cantidad = Convert.ToInt32(Console.ReadLine());
-                for (int i = 1; i <= cantidad; i++)
+                Console.WriteLine($"\nEmpleado [{i}]:");
+                Empleado empleado = new Empleado();
+                if (empleado.Cargar())
                 {
-                    Console.WriteLine($"\nEmpleado [{i}]:");
-                    Empleado empleado = new Empleado();
-                    if (empleado.Cargar())
-                    {
-                        listaEmpleados.Add(empleado);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Error al cargar el empleado. Intente nuevamente");
-                        i--;
-                    }
+                    listaEmpleados.Add(empleado);
                 }
-                Console.WriteLine("\nFin de la carga.");
-                return true;
+                else
+                {
+                    Console.WriteLine("Error al cargar el empleado. Intente nuevamente");
+                    i--;
+                }
             }
-            catch (Exception exNumero)
-            {
-                Console.WriteLine("ERROR: El número ingresado no es válido");
-                Console.WriteLine("Intente nuevamente");
-                //Console.WriteLine(exNumero); // activar si quiero mostrar el error
-                Logger.Error(exNumero, "ERROR: El número ingresado no es válido");
-                return false;
-            }
+            Console.WriteLine("\nFin de la carga.");
+            return true;
         }
-
-        static void MostrarEmpleados(List<Empleado> listaEmpleados)
+        catch (Exception exNumero)
         {
-            Mensajes.TerminarLinea("Mostrando empleados:");
-            foreach (Empleado emple in listaEmpleados)
-            {
-                Console.WriteLine($"\nEmpleado [{listaEmpleados.IndexOf(emple) + 1}]:");
-                emple.Mostrar();
-            }
-            Console.WriteLine("\nFin de la lista.");
+            Console.WriteLine("ERROR: El número ingresado no es válido");
+            Console.WriteLine("Intente nuevamente");
+            //Console.WriteLine(exNumero); // activar si quiero mostrar el error
+            Logger.Error(exNumero, "ERROR: El número ingresado no es válido");
+            return false;
         }
     }
 
+    static void MostrarEmpleados(List<Empleado> listaEmpleados)
+    {
+        Mensajes.TerminarLinea("Mostrando empleados:");
+        foreach (Empleado emple in listaEmpleados)
+        {
+            Console.WriteLine($"\nEmpleado [{listaEmpleados.IndexOf(emple) + 1}]:");
+            emple.Mostrar();
+        }
+        Console.WriteLine("\nFin de la lista.");
+    }
 }
+
